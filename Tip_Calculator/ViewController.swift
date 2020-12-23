0d56728ecb4279c5bd36d1d61875c4a95c3f57db
UgochukwuNmz <68568717+UgochukwuNmz@users.noreply.github.com>
2020-12-23 15:08:21 -0600

    better design

--- Tip_Calculator/ViewController.swift
+++ Tip_Calculator/ViewController.swift
@@ -8,38 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var tipLabel: UILabel!
     
-    @IBOutlet weak var totalLabel: UILabel!
-    
+    var value: Float = 0.2
+    @IBOutlet var tipPercent: UILabel!
     @IBOutlet weak var billField: UITextField!
-    
-    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var total: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        value = round(sender.value * 100) / 100
+        tipPercent.text = String(format: "%.0f", round(sender.value * 100)) + "%"
+    }
+    
     @IBAction func calculateTIp(_ sender: Any) {
         // Get the bill amount
-        let bill = Double(billField.text!) ?? 0
-        // ?? 0 means if it's not valid, change it to 0
+        let bill = Float(billField.text!) ?? 0
         
-        // Calculate the tip and total
-        let tipPercentages = [0.15, 0.18, 0.2]
-        
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
-        
+        let tip = round(bill * value)
+        let tempTotal = bill + tip
+
         // Update the tip and total labels
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        total.text = String(format: "$%.2f", tempTotal)
     }
+    
 }
 
